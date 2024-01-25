1fcf51a9bf3c14320caf9deff98e9c11ff624d35
bradschwartz <baschwartz95@gmail.com>
2024-01-24 16:58:12 -0800

    adding dialyzer to test env for CII

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule GraphqlMarkdown.MixProject do
   defp deps do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.30", only: [:dev, :test], runtime: false},
       {:jason, "~> 1.4"}
     ]
